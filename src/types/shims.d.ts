35c0471cdb837266f8534a3c4f98a21c724111a8
Deeathwiing <andreykorotkins@gmail.com>
2020-12-13 21:19:16 +0300

    fix: fix prev commit and git

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,3 +1,7 @@
+import { type } from 'os';
+
+import Country from '../components/List/Country/Country';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.scss' {
   const classes: { [key: string]: string };
@@ -8,3 +12,40 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
+
+export interface ResponseType {
+  data: Data;
+}
+
+export type Data = {
+  Message: string;
+  Global: Global;
+  Countries: Array<CountryType>;
+};
+
+export type Global = {
+  NewConfirmed: number;
+  TotalConfirmed: number;
+  NewDeaths: number;
+  TotalDeaths: number;
+  NewRecovered: number;
+  TotalRecovered: number;
+};
+
+export type CountryType = {
+  Country: string;
+  CountryCode: string;
+  Slug: string;
+  NewConfirmed: number;
+  TotalConfirmed: number;
+  NewDeaths: number;
+  TotalDeaths: number;
+  NewRecovered: number;
+  TotalRecovered: number;
+  Date: string;
+};
+
+export type ActionType = {
+  type: string;
+  data: Global;
+};
