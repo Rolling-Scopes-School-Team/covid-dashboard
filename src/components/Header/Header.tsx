59c721887ef85877368ba21d08ea570bd0a62e38
Boris Krasko <boris.krasko@icloud.com>
2020-12-15 01:27:08 +0100

    feat: add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import './index.scss';
 
-const Header = (): JSX.Element => <div>Test</div>;
+import styles from './index.scss';
+
+const Header = (): JSX.Element => (
+  <header className="header">
+    <div className={styles.title}>COVID-19 Dashboard</div>
+    <div className={styles.subtitle}>
+      by the Center for Systems Science and Engineering (CSSE)
+      <br />
+      at Johns Hopkins University (JHU)
+    </div>
+  </header>
+);
 
 export default Header;
