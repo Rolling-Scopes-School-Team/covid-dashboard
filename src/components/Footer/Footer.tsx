cca541beb759b1f24714fa78ad5bf5a302d21a85
Boris Krasko <boris.krasko@icloud.com>
2020-12-22 18:40:10 +0100

    feat: add basic layout to Footer (html => jsx)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,43 @@ import React from 'react';
 // import styles from '@/components/Footer/index.scss';
 // import classes from '@/components/index.scss';
 
-const Footer = (): JSX.Element => <footer>TEST</footer>;
+const Footer = (): JSX.Element => (
+  <footer className="container">
+    <div className="logo">
+      {/* <pre>R{"        "}S{"\n"}SC HO OL{"\n"}{"        "}</pre> */}
+      RS SCHOOL
+    </div>
+    <div className="github-links">
+      <ul>
+        App developers:
+        <li>
+          <a href="https://github.com/">
+            {/* <object type="image/svg+xml" data="github.svg" className="icon-github" /> */}
+            Git account
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/">
+            {/* <object type="image/svg+xml" data="github.svg" className="icon-github" /> */}
+            Git account
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/">
+            {/* <object type="image/svg+xml" data="github.svg" className="icon-github" /> */}
+            Git account
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/">
+            {/* <object type="image/svg+xml" data="github.svg" className="icon-github" /> */}
+            Git account
+          </a>
+        </li>
+      </ul>
+      <span className="year">2020</span>
+    </div>
+  </footer>
+);
 
 export default Footer;
