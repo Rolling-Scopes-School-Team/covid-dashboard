06cfa70d77d79577d98f3c26449f7a38b780f80f
Boris Krasko <boris.krasko@icloud.com>
2020-12-25 20:06:41 +0100

    fix: flags

--- src/components/List/Country/Country.tsx
+++ src/components/List/Country/Country.tsx
@@ -27,20 +27,24 @@ const Country: React.FC<CountryState> = ({ countries, selected, options }) => {
           onClick={() => handleClick(element)}
           className={classes['country-hover']}
         >
-          <span className={classes['counter']}>
-            {(selected === options[0][0] && element.cases) ||
-              (selected === options[1][0] && element.todayCases) ||
-              (selected === options[2][0] && element.todayDeaths) ||
-              (selected === options[3][0] && element.deaths) ||
-              (selected === options[4][0] && element.todayRecovered) ||
-              (selected === options[5][0] && element.recovered)}
+          <span>
+            <span className={classes['counter']}>
+              {(selected === options[0][0] && element.cases) ||
+                (selected === options[1][0] && element.todayCases) ||
+                (selected === options[2][0] && element.todayDeaths) ||
+                (selected === options[3][0] && element.deaths) ||
+                (selected === options[4][0] && element.todayRecovered) ||
+                (selected === options[5][0] && element.recovered)}
+            </span>
+            <span className={styles['country']}>{element.country}</span>
           </span>
-          <span className={classes['country']}>{element.country}</span>
-          <img
-            className={styles['country-flag']}
-            src={element.countryInfo.flag}
-            alt={element.country}
-          />
+          <span className={styles['country-flag']}>
+            <img
+              className={styles['country-flag-image']}
+              src={element.countryInfo.flag}
+              alt={element.country}
+            />
+          </span>
         </li>
       ))}
     </React.Fragment>
